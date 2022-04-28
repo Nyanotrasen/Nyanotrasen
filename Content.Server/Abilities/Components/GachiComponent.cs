702abafec622e7c9372552f27b827fb4e7bcc8d6
Rane <elijahrane@gmail.com>
2022-04-28 01:04:42 -0400

    Have all 3 situations and a bunch of sounds

--- Content.Server/Abilities/Components/GachiComponent.cs
+++ Content.Server/Abilities/Components/GachiComponent.cs
@@ -1,6 +1,15 @@
+using Content.Shared.Sound;
+
 namespace Content.Server.Abilities.Gachi.Components
 {
     [RegisterComponent]
     public sealed class GachiComponent : Component
-    {}
+    {
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField("painSound")]
+        public SoundSpecifier PainSound { get; set; } = new SoundCollectionSpecifier("GachiPain");
+
+        [DataField("hitOtherSound")]
+        public SoundSpecifier HitOtherSound { get; set; } = new SoundCollectionSpecifier("GachiHitOther");
+    }
 }
--- Content.Server/Abilities/GachiSystem.cs
+++ Content.Server/Abilities/GachiSystem.cs
@@ -2,25 +2,55 @@ using Content.Shared.Damage;
 using Content.Shared.Inventory.Events;
 using Content.Server.Abilities.Gachi.Components;
 using Content.Server.Clothing.Components;
+using Content.Server.Weapon.Melee;
+using Content.Shared.MobState;
 using Robust.Shared.Audio;
 using Robust.Shared.Player;
+using Robust.Shared.Random;
 
 namespace Content.Server.Abilities.Gachi
 {
     public sealed class GachiSystem : EntitySystem
     {
+        [Dependency] private readonly IRobustRandom _random = default!;
+
         public override void Initialize()
         {
             base.Initialize();
             SubscribeLocalEvent<GachiComponent, DamageChangedEvent>(OnDamageChanged);
+            SubscribeLocalEvent<GachiComponent, MeleeHitEvent>(OnMeleeHit);
+            SubscribeLocalEvent<GachiComponent, MobStateChangedEvent>(OnMobStateChanged);
             SubscribeLocalEvent<JabroniOutfitComponent, GotEquippedEvent>(OnEquipped);
             SubscribeLocalEvent<JabroniOutfitComponent, GotUnequippedEvent>(OnUnequipped);
         }
 
         private void OnDamageChanged(EntityUid uid, GachiComponent component, DamageChangedEvent args)
         {
-            if (args.DamageIncreased && args.DamageDelta != null && args.DamageDelta.Total >= 5)
-                SoundSystem.Play(Filter.Pvs(uid), "/Audio/Effects/Gachi/van_fuckyou.ogg", uid);
+            if (args.DamageIncreased && args.DamageDelta != null && args.DamageDelta.Total >= 5 && _random.Prob(0.3f))
+            {
+                if (_random.Prob(0.01f))
+                {
+                    SoundSystem.Play(Filter.Pvs(uid), "/Audio/Effects/Gachi/ripears.ogg", uid, AudioParams.Default.WithVolume(8f));
+                    return;
+                }
+                SoundSystem.Play(Filter.Pvs(uid), component.PainSound.GetSound(), uid);
+            }
+        }
+
+        private void OnMeleeHit(EntityUid uid, GachiComponent component, MeleeHitEvent args)
+        {
+            if (_random.Prob(0.2f))
+            {
+                SoundSystem.Play(Filter.Pvs(uid), component.HitOtherSound.GetSound(), uid);
+            }
+        }
+
+        private void OnMobStateChanged(EntityUid uid, GachiComponent component, MobStateChangedEvent args)
+        {
+            if (args.CurrentMobState.IsCritical())
+            {
+                SoundSystem.Play(Filter.Pvs(uid), "/Audio/Effects/Gachi/knockedhimout.ogg", uid);
+            }
         }
 
         private void OnEquipped(EntityUid uid, JabroniOutfitComponent component, GotEquippedEvent args)
