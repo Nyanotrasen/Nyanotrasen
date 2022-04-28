340f12822f18fa5169be8e86c7370833dddffcc1
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-04-28 02:37:07 -0400

    Secret pr don't look (#17)
    * First prototype
    
    * Add jabroni outfit
    
    * Have all 3 situations and a bunch of sounds
    
    * Finish sound playing finally
    
    * Update atlas for change
    
    * More gachi sounds

--- Content.Client/Administration/BwoinkSystem.cs
+++ Content.Client/Administration/BwoinkSystem.cs
@@ -1,5 +1,4 @@
 ﻿#nullable enable
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Content.Client.Administration.Managers;
@@ -10,12 +9,10 @@ using JetBrains.Annotations;
 using Robust.Client.Graphics;
 using Robust.Client.UserInterface.CustomControls;
 using Robust.Client.Player;
-using Robust.Shared.GameObjects;
 using Robust.Shared.Player;
-using Robust.Shared.Localization;
 using Robust.Shared.Audio;
-using Robust.Shared.IoC;
 using Robust.Shared.Network;
+using Robust.Shared.Random;
 
 namespace Content.Client.Administration
 {
@@ -25,6 +22,8 @@ namespace Content.Client.Administration
         [Dependency] private readonly IClientAdminManager _adminManager = default!;
         [Dependency] private readonly IPlayerManager _playerManager = default!;
         [Dependency] private readonly IClyde _clyde = default!;
+
+        [Dependency] private readonly IRobustRandom _random = default!;
 
         private BwoinkWindow? _adminWindow;
         private DefaultWindow? _plainWindow;
@@ -41,6 +40,8 @@ namespace Content.Client.Administration
             var localPlayer = _playerManager.LocalPlayer;
             if (localPlayer?.UserId != message.TrueSender)
             {
+                if (_random.Prob(0.05f))
+                    SoundSystem.Play(Filter.Local(), "/Audio/Effects/Gachi/whatthehellareyoutwodoing.ogg");
                 SoundSystem.Play(Filter.Local(), "/Audio/Effects/adminhelp.ogg");
                 _clyde.RequestWindowAttention();
             }
