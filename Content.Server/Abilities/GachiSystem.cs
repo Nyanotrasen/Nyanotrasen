dcd7519fa9b865723a9c8581039740bfc9eff759
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-04-28 02:28:42 -0400

    Add Jabroni outfit (#16)
    * First prototype
    
    * Add jabroni outfit
    
    * Have all 3 situations and a bunch of sounds
    
    * Finish sound playing finally
    
    * Update atlas for change

---
+++ Content.Server/Abilities/GachiSystem.cs
@@ -0,0 +1,96 @@
+using Content.Shared.Damage;
+using Content.Shared.FixedPoint;
+using Content.Shared.Inventory.Events;
+using Content.Server.Abilities.Gachi.Components;
+using Content.Server.Clothing.Components;
+using Content.Server.Weapon.Melee;
+using Content.Shared.MobState;
+using Robust.Shared.Audio;
+using Robust.Shared.Player;
+using Robust.Shared.Random;
+
+namespace Content.Server.Abilities.Gachi
+{
+    public sealed class GachiSystem : EntitySystem
+    {
+        [Dependency] private readonly IRobustRandom _random = default!;
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<GachiComponent, DamageChangedEvent>(OnDamageChanged);
+            SubscribeLocalEvent<GachiComponent, MeleeHitEvent>(OnMeleeHit);
+            SubscribeLocalEvent<GachiComponent, MobStateChangedEvent>(OnMobStateChanged);
+            SubscribeLocalEvent<JabroniOutfitComponent, GotEquippedEvent>(OnEquipped);
+            SubscribeLocalEvent<JabroniOutfitComponent, GotUnequippedEvent>(OnUnequipped);
+        }
+
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+            foreach (var gachi in EntityQuery<GachiComponent>())
+            {
+                gachi.Accumulator += frameTime;
+                if (gachi.Accumulator < gachi.AddToMultiplierTime.TotalSeconds)
+                    continue;
+                gachi.Accumulator -= (float) gachi.AddToMultiplierTime.TotalSeconds;
+                if (gachi.Multiplier < 1f)
+                    gachi.Multiplier += 0.01f;
+            }
+        }
+
+        private void OnDamageChanged(EntityUid uid, GachiComponent component, DamageChangedEvent args)
+        {
+            if (TryComp<DamageableComponent>(uid, out var damageableComponent) && (damageableComponent.TotalDamage + args.DamageDelta?.Total >= 100))
+                return;
+            if (args.DamageIncreased && args.DamageDelta != null && args.DamageDelta.Total >= 5 && _random.Prob(0.3f * component.Multiplier))
+            {
+                FixedPoint2 newMultiplier = component.Multiplier - 0.25;
+                component.Multiplier = (float) FixedPoint2.Max(FixedPoint2.Zero, newMultiplier);
+
+                if (_random.Prob(0.01f))
+                {
+                    SoundSystem.Play(Filter.Pvs(uid), "/Audio/Effects/Gachi/ripears.ogg", uid, AudioParams.Default.WithVolume(8f));
+                    return;
+                }
+                SoundSystem.Play(Filter.Pvs(uid), component.PainSound.GetSound(), uid);
+
+            }
+        }
+
+        private void OnMeleeHit(EntityUid uid, GachiComponent component, MeleeHitEvent args)
+        {
+            if (_random.Prob(0.2f * component.Multiplier))
+            {
+                FixedPoint2 newMultiplier = component.Multiplier - 0.25;
+                component.Multiplier = (float) FixedPoint2.Max(FixedPoint2.Zero, newMultiplier);
+                SoundSystem.Play(Filter.Pvs(uid), component.HitOtherSound.GetSound(), uid);
+            }
+        }
+
+        private void OnMobStateChanged(EntityUid uid, GachiComponent component, MobStateChangedEvent args)
+        {
+            if (args.CurrentMobState.IsCritical())
+            {
+                SoundSystem.Play(Filter.Pvs(uid), "/Audio/Effects/Gachi/knockedhimout.ogg", uid);
+            }
+        }
+
+        private void OnEquipped(EntityUid uid, JabroniOutfitComponent component, GotEquippedEvent args)
+        {
+            if (!TryComp<ClothingComponent>(uid, out var clothing))
+                return;
+            if (!clothing.SlotFlags.HasFlag(args.SlotFlags))
+                return;
+            EnsureComp<GachiComponent>(args.Equipee);
+            component.IsActive = true;
+        }
+
+        private void OnUnequipped(EntityUid uid, JabroniOutfitComponent component, GotUnequippedEvent args)
+        {
+            if (!component.IsActive)
+                return;
+            component.IsActive = false;
+            RemComp<GachiComponent>(uid);
+        }
+    }
+}
