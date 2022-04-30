ad728f3071d689eb770da910c4d18e7e84c22c5a
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-04-30 02:19:00 -0400

    Vomiting sound is now positional (#23)

--- Content.Server/Medical/VomitSystem.cs
+++ Content.Server/Medical/VomitSystem.cs
@@ -39,7 +39,7 @@ namespace Content.Server.Medical
 
             var puddleComp = Comp<PuddleComponent>(puddle);
 
-            SoundSystem.Play(Filter.Pvs(uid), "/Audio/Effects/Diseases/vomiting.ogg", AudioHelpers.WithVariation(0.2f).WithVolume(-4f));
+            SoundSystem.Play(Filter.Pvs(uid), "/Audio/Effects/Diseases/vomiting.ogg", uid, AudioHelpers.WithVariation(0.2f).WithVolume(-4f));
 
             _popupSystem.PopupEntity(Loc.GetString("disease-vomit", ("person", uid)), uid, Filter.Pvs(uid));
 
