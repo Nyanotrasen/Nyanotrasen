24f843a65bb5dcfc65a292864c6fbd367cd85436
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-05-13 21:58:39 -0400

    Shrink ray fix (#104)
    * You can use a shrink ray on yourself
    
    * Fix afterinteract apply item

--- Content.Server/Nyanotrasen/ShrinkRay/ShrinkRayComponent.cs
+++ Content.Server/Nyanotrasen/ShrinkRay/ShrinkRayComponent.cs
@@ -3,7 +3,8 @@ namespace Content.Server.ShrinkRay
     [RegisterComponent]
     public sealed class ShrinkRayComponent : Component
     {
-        public bool ApplyItem = false;
+        [DataField("applyItem")]
+        public bool ApplyItem = true;
 
         [DataField("scaleFactor")]
         public Vector2 ScaleFactor = (0.3f, 0.3f);
