185fcabb181a9ed870ecf2a44f9c8ea4f961bfe3
Rane <elijahrane@gmail.com>
2022-05-19 00:00:46 -0400

    Basics working

--- Content.Server/Nyanotrasen/Carrying/CarryingSystem.cs
+++ Content.Server/Nyanotrasen/Carrying/CarryingSystem.cs
@@ -1,7 +1,9 @@
 using Content.Server.Hands.Systems;
 using Content.Server.Hands.Components;
-using Content.Shared.Hands.EntitySystems;
+using Content.Shared.MobState.Components;
 using Content.Shared.Hands.Components;
+using Content.Shared.Hands;
+using Content.Shared.Stunnable;
 using Content.Shared.Verbs;
 using Robust.Shared.Containers;
 
@@ -16,6 +18,7 @@ namespace Content.Server.Carrying
         {
             base.Initialize();
             SubscribeLocalEvent<CarriableComponent, GetVerbsEvent<AlternativeVerb>>(AddCarryVerb);
+            SubscribeLocalEvent<CarryingComponent, VirtualItemDeletedEvent>(OnVirtualItemDeleted);
         }
 
 
@@ -27,21 +30,46 @@ namespace Content.Server.Carrying
             if (!CanCarry(args.User, uid, component))
                 return;
 
+            if (HasComp<CarryingComponent>(args.User)) // yeah not dealing with that
+                return;
+
+            if (!HasComp<KnockedDownComponent>(uid) && !(TryComp<MobStateComponent>(uid, out var state) && (state.IsCritical() || state.IsDead() || state.IsIncapacitated())))
+                return;
+
             AlternativeVerb verb = new()
             {
                 Act = () =>
                 {
                     Carry(args.User, uid);
                 },
-                Text = Loc.GetString("bible-summon-verb"),
+                Text = Loc.GetString("carry-verb"),
                 Priority = 2
             };
             args.Verbs.Add(verb);
         }
 
+        private void OnVirtualItemDeleted(EntityUid uid, CarryingComponent component, VirtualItemDeletedEvent args)
+        {
+            if (!HasComp<CarriableComponent>(args.BlockingEntity))
+                return;
+
+            DropCarried(uid, args.BlockingEntity);
+        }
+
         private void Carry(EntityUid carrier, EntityUid carried)
         {
+            Transform(carried).Coordinates = Transform(carrier).Coordinates;
             Transform(carried).ParentUid = carrier;
+            _virtualItemSystem.TrySpawnVirtualItemInHand(carried, carrier);
+            _virtualItemSystem.TrySpawnVirtualItemInHand(carried, carrier);
+            EnsureComp<CarryingComponent>(carrier);
+        }
+
+        private void DropCarried(EntityUid carrier, EntityUid carried)
+        {
+            RemComp<CarryingComponent>(carrier); // get rid of this first so we don't recusrively fire that event
+            _virtualItemSystem.DeleteInHandsMatching(carrier, carried);
+            Transform(carried).AttachToGridOrMap();
         }
 
         public bool CanCarry(EntityUid carrier, EntityUid carried, CarriableComponent? carriedComp = null)
