f983b75b37d9c1a5a7b34ab4b12d1824dee4fa49
Rane <elijahrane@gmail.com>
2022-05-21 00:50:45 -0400

    Popups

--- Content.Server/Nyanotrasen/AI/EntitySystems/InjectNearbySystem.cs
+++ Content.Server/Nyanotrasen/AI/EntitySystems/InjectNearbySystem.cs
@@ -1,8 +1,10 @@
 using Content.Server.Chemistry.Components.SolutionManager;
 using Content.Server.Chemistry.EntitySystems;
 using Content.Server.AI.Tracking;
+using Content.Server.Popups;
 using Content.Shared.MobState.Components;
 using Content.Shared.Damage;
+using Robust.Shared.Player;
 
 namespace Content.Server.AI.EntitySystems
 {
@@ -10,6 +12,7 @@ namespace Content.Server.AI.EntitySystems
     {
         [Dependency] private readonly EntityLookupSystem _lookup = default!;
         [Dependency] private readonly SolutionContainerSystem _solutionSystem = default!;
+        [Dependency] private readonly PopupSystem _popupSystem = default!;
 
         public EntityUid GetNearbyInjectable(EntityUid medibot, float range = 4)
         {
@@ -37,6 +40,7 @@ namespace Content.Server.AI.EntitySystems
             {
                 _solutionSystem.TryAddReagent(target, injectable, "Tricordrazine", 15, out var accepted);
                 EnsureComp<RecentlyInjectedComponent>(target);
+                _popupSystem.PopupEntity(Loc.GetString("hypospray-component-feel-prick-message"), target, Filter.Entities(target));
                 return true;
             }
 
@@ -44,6 +48,7 @@ namespace Content.Server.AI.EntitySystems
             {
                 _solutionSystem.TryAddReagent(target, injectable, "Inaprovaline", 15, out var accepted);
                 EnsureComp<RecentlyInjectedComponent>(target);
+                _popupSystem.PopupEntity(Loc.GetString("hypospray-component-feel-prick-message"), target, Filter.Entities(target));
                 return true;
             }
 
