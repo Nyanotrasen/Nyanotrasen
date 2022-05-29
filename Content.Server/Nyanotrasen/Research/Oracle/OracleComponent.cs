78f93b9cd53b09f28d25c9dc7bc1c139e010c02d
Rane <elijahrane@gmail.com>
2022-05-29 12:05:04 -0400

    oracles now bark more often + keep track of their last desired item

--- Content.Server/Nyanotrasen/Research/Oracle/OracleComponent.cs
+++ Content.Server/Nyanotrasen/Research/Oracle/OracleComponent.cs
@@ -11,15 +11,18 @@ namespace Content.Server.Research.Oracle
 
         [ViewVariables]
         [DataField("resetTime")]
-        public TimeSpan ResetTime = TimeSpan.FromMinutes(10);
+        public TimeSpan ResetTime = TimeSpan.FromMinutes(1);
 
         [DataField("barkAccumulator")]
         public float BarkAccumulator = 0f;
 
         [DataField("barkTime")]
-        public TimeSpan BarkTime = TimeSpan.FromSeconds(40);
+        public TimeSpan BarkTime = TimeSpan.FromSeconds(30);
 
         [ViewVariables(VVAccess.ReadWrite)]
         public EntityPrototype DesiredPrototype = default!;
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        public EntityPrototype? LastDesiredPrototype = default!;
     }
 }
