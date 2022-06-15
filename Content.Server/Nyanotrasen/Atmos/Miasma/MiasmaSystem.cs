b01847cb469393dae25ee5333961768ec865f1c3
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-06-14 23:33:39 -0400

    Miasma (#201)
    * starting point
    
    * cause disease """works***
    
    * ChemCauseRandomDisease
    
    * Threshold
    
    * miasma time
    
    * make all mobs perishable

---
+++ Content.Server/Nyanotrasen/Atmos/Miasma/MiasmaSystem.cs
@@ -0,0 +1,76 @@
+using Content.Shared.MobState;
+using Content.Server.Atmos.EntitySystems;
+using Content.Server.Temperature.Components;
+using Robust.Shared.Physics;
+using Robust.Shared.Containers;
+
+namespace Content.Server.Atmos.Miasma
+{
+    public sealed class MiasmaSystem : EntitySystem
+    {
+        [Dependency] private readonly AtmosphereSystem _atmosphereSystem = default!;
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+            foreach (var (rotting, perishable) in EntityQuery<RottingComponent, PerishableComponent>())
+            {
+                if (!perishable.Progressing)
+                    continue;
+
+                if (TryComp<TemperatureComponent>(perishable.Owner, out var temp) && temp.CurrentTemperature < 274f)
+                    continue;
+
+                perishable.DeathAccumulator += frameTime;
+                if (perishable.DeathAccumulator < perishable.RotAfter.TotalSeconds)
+                    continue;
+
+                perishable.RotAccumulator += frameTime;
+                if (perishable.RotAccumulator < 1f)
+                    continue;
+
+                perishable.RotAccumulator -= 1f;
+
+                if (!TryComp<FixturesComponent>(perishable.Owner, out var fixtures))
+                    continue;
+
+                var mass = 0f;
+
+                foreach (var fixture in fixtures.Fixtures.Values)
+                {
+                    mass += fixture.Mass;
+                }
+
+                var tileMix = _atmosphereSystem.GetTileMixture(Transform(perishable.Owner).Coordinates);
+                if (tileMix != null)
+                    tileMix.AdjustMoles(6, perishable.MolsPerSecondPerUnitMass * mass);
+            }
+        }
+
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<PerishableComponent, MobStateChangedEvent>(OnMobStateChanged);
+            SubscribeLocalEvent<NoRotContainerComponent, EntInsertedIntoContainerMessage>(OnEntInserted);
+            SubscribeLocalEvent<NoRotContainerComponent, EntRemovedFromContainerMessage>(OnEntRemoved);
+        }
+
+        private void OnMobStateChanged(EntityUid uid, PerishableComponent component, MobStateChangedEvent args)
+        {
+            if (args.Component.IsDead())
+                EnsureComp<RottingComponent>(uid);
+        }
+
+        private void OnEntInserted(EntityUid uid, NoRotContainerComponent component, EntInsertedIntoContainerMessage args)
+        {
+            if (TryComp<PerishableComponent>(args.Entity, out var perishable))
+                perishable.Progressing = false;
+        }
+
+        private void OnEntRemoved(EntityUid uid, NoRotContainerComponent component, EntRemovedFromContainerMessage args)
+        {
+            if (TryComp<PerishableComponent>(args.Entity, out var perishable))
+                perishable.Progressing = true;
+        }
+    }
+}
