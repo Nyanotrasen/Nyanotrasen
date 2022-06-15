8960f0aa9126941f46f8b31e7557a00f29a4cdf9
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-06-15 01:30:14 -0400

    Bodies rot and gib eventually and freezers work (#220)
    * miasma gibbing
    
    * Miasma explosion
    
    * thanks emo

--- Content.Server/Nyanotrasen/Atmos/Miasma/MiasmaSystem.cs
+++ Content.Server/Nyanotrasen/Atmos/Miasma/MiasmaSystem.cs
@@ -1,6 +1,8 @@
 using Content.Shared.MobState;
+using Content.Shared.Damage;
 using Content.Server.Atmos.EntitySystems;
 using Content.Server.Temperature.Components;
+using Content.Server.Body.Components;
 using Robust.Shared.Physics;
 using Robust.Shared.Containers;
 
@@ -9,6 +11,8 @@ namespace Content.Server.Atmos.Miasma
     public sealed class MiasmaSystem : EntitySystem
     {
         [Dependency] private readonly AtmosphereSystem _atmosphereSystem = default!;
+        [Dependency] private readonly DamageableSystem _damageableSystem = default!;
+
         public override void Update(float frameTime)
         {
             base.Update(frameTime);
@@ -29,6 +33,12 @@ namespace Content.Server.Atmos.Miasma
                     continue;
 
                 perishable.RotAccumulator -= 1f;
+
+                DamageSpecifier damage = new();
+                damage.DamageDict.Add("Blunt", 0.25); // Slowly accumulate enough to explode after like half an hour
+                damage.DamageDict.Add("Cellular", 0.25); // Cloning rework might use this eventually
+
+                _damageableSystem.TryChangeDamage(perishable.Owner, damage, true, true);
 
                 if (!TryComp<FixturesComponent>(perishable.Owner, out var fixtures))
                     continue;
@@ -51,6 +61,7 @@ namespace Content.Server.Atmos.Miasma
         {
             base.Initialize();
             SubscribeLocalEvent<PerishableComponent, MobStateChangedEvent>(OnMobStateChanged);
+            SubscribeLocalEvent<PerishableComponent, BeingGibbedEvent>(OnGibbed);
             SubscribeLocalEvent<NoRotContainerComponent, EntInsertedIntoContainerMessage>(OnEntInserted);
             SubscribeLocalEvent<NoRotContainerComponent, EntRemovedFromContainerMessage>(OnEntRemoved);
         }
@@ -59,6 +70,24 @@ namespace Content.Server.Atmos.Miasma
         {
             if (args.Component.IsDead())
                 EnsureComp<RottingComponent>(uid);
+        }
+
+        private void OnGibbed(EntityUid uid, PerishableComponent component, BeingGibbedEvent args)
+        {
+                if (!TryComp<FixturesComponent>(uid, out var fixtures))
+                    return;
+
+                var mass = 0f;
+
+                foreach (var fixture in fixtures.Fixtures.Values)
+                {
+                    mass += fixture.Mass;
+                }
+
+                var molsToDump = (component.MolsPerSecondPerUnitMass * mass) * component.DeathAccumulator;
+                var tileMix = _atmosphereSystem.GetTileMixture(Transform(uid).Coordinates);
+                if (tileMix != null)
+                    tileMix.AdjustMoles(6, molsToDump);
         }
 
         private void OnEntInserted(EntityUid uid, NoRotContainerComponent component, EntInsertedIntoContainerMessage args)
