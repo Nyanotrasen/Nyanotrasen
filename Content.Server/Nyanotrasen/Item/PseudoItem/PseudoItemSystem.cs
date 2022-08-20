3e9941078f27a402cc55a8261991d7074c8620f0
Rane <elijahrane@gmail.com>
2022-08-20 15:20:56 -0400

    okay, removing works

--- Content.Server/Nyanotrasen/Item/PseudoItem/PseudoItemSystem.cs
+++ Content.Server/Nyanotrasen/Item/PseudoItem/PseudoItemSystem.cs
@@ -1,18 +1,21 @@
 using Content.Server.Storage.Components;
 using Content.Server.Storage.EntitySystems;
 using Content.Shared.Verbs;
-using Content.Shared.Storage;
 using Content.Shared.Item;
+using Robust.Shared.Containers;
 
 namespace Content.Server.Item.PseudoItem
 {
     public sealed class PseudoItemSystem : EntitySystem
     {
         [Dependency] private readonly StorageSystem _storageSystem = default!;
+        [Dependency] private readonly ItemSystem _itemSystem = default!;
         public override void Initialize()
         {
             base.Initialize();
             SubscribeLocalEvent<PseudoItemComponent, GetVerbsEvent<InnateVerb>>(AddInsertVerb);
+            SubscribeLocalEvent<PseudoItemComponent, EntGotRemovedFromContainerMessage>(OnEntRemoved);
+            SubscribeLocalEvent<PseudoItemComponent, GettingPickedUpAttemptEvent>(OnGettingPickedUpAttempt);
         }
 
         private void AddInsertVerb(EntityUid uid, PseudoItemComponent component, GetVerbsEvent<InnateVerb> args)
@@ -36,8 +39,20 @@ namespace Content.Server.Item.PseudoItem
                 Priority = 2
             };
             args.Verbs.Add(verb);
+        }
 
+        private void OnEntRemoved(EntityUid uid, PseudoItemComponent component, EntGotRemovedFromContainerMessage args)
+        {
+            RemComp<ItemComponent>(uid);
+        }
 
+        private void OnGettingPickedUpAttempt(EntityUid uid, PseudoItemComponent component, GettingPickedUpAttemptEvent args)
+        {
+            if (args.User == args.Item)
+                return;
+
+            Transform(uid).AttachToGridOrMap();
+            args.Cancel();
         }
 
         public void TryInsert(EntityUid toInsert, PseudoItemComponent component, ServerStorageComponent storage)
@@ -46,6 +61,7 @@ namespace Content.Server.Item.PseudoItem
                 return;
 
             var item = EnsureComp<ItemComponent>(toInsert);
+            _itemSystem.SetSize(toInsert, component.Size, item);
 
             _storageSystem.Insert(storage.Owner, toInsert, storage);
         }
