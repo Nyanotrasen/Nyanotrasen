1f4b0b505493d0c732cc3267bf643ccc08029eed
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-08-20 16:05:33 -0400

    Felinids fit in duffel bags (#393)
    * initial test
    
    * okay, removing works
    
    * works on my machines

---
+++ Content.Server/Nyanotrasen/Item/PseudoItem/PseudoItemComponent.cs
@@ -0,0 +1,17 @@
+using System.Threading;
+
+namespace Content.Server.Item.PseudoItem
+{
+    /// <summary>
+    /// For entities that behave like an item under certain conditions,
+    /// but not under most conditions.
+    /// </summary>
+    [RegisterComponent]
+    public sealed class PseudoItemComponent : Component
+    {
+        public CancellationTokenSource? CancelToken;
+
+        [DataField("size")]
+        public int Size = 120;
+    }
+}
