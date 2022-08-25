e19940db457032ba8a41fe6e8696426826c84f8f
Rane <60792108+Elijahrane@users.noreply.github.com>
2022-08-24 20:58:28 -0400

    dispel behavior for holos (#414)

--- Content.Server/Nyanotrasen/Abilities/Psionics/Abilities/DispelPowerSystem.cs
+++ Content.Server/Nyanotrasen/Abilities/Psionics/Abilities/DispelPowerSystem.cs
@@ -3,6 +3,8 @@ using Content.Shared.Actions.ActionTypes;
 using Content.Shared.StatusEffect;
 using Content.Shared.Abilities.Psionics;
 using Content.Shared.Popups;
+using Content.Shared.Damage;
+using Content.Server.Guardian;
 using Content.Server.Popups;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Player;
@@ -14,7 +16,8 @@ namespace Content.Server.Abilities.Psionics
         [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
         [Dependency] private readonly StatusEffectsSystem _statusEffects = default!;
         [Dependency] private readonly SharedActionsSystem _actions = default!;
-
+        [Dependency] private readonly DamageableSystem _damageableSystem = default!;
+        [Dependency] private readonly GuardianSystem _guardianSystem = default!;
         [Dependency] private readonly PopupSystem _popupSystem = default!;
 
         public override void Initialize()
@@ -47,6 +50,18 @@ namespace Content.Server.Abilities.Psionics
         {
             if (!HasComp<DispellableComponent>(args.Target))
                 return;
+
+            if (TryComp<GuardianComponent>(args.Target, out var guardian))
+            {
+                DamageSpecifier damage = new();
+                damage.DamageDict.Add("Blunt", 100);
+                if (TryComp<GuardianHostComponent>(guardian.Host, out var host))
+                    _guardianSystem.ToggleGuardian(host);
+
+                _damageableSystem.TryChangeDamage(args.Target, damage, true, true);
+                return;
+            }
+
             EntityManager.QueueDeleteEntity(args.Target);
             Spawn("Ash", Transform(args.Target).Coordinates);
             _popupSystem.PopupEntity(Loc.GetString("admin-smite-turned-ash-other", ("name", args.Target)), args.Target,
