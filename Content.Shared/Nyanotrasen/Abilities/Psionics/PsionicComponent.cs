853d83c7c1467ed0661804929b7e928c368fe956
Rane <elijahrane@gmail.com>
2022-09-23 04:02:39 -0400

    make psionic networked

--- Content.Shared/Nyanotrasen/Abilities/Psionics/PsionicComponent.cs
+++ Content.Shared/Nyanotrasen/Abilities/Psionics/PsionicComponent.cs
@@ -1,8 +1,9 @@
 using Content.Shared.Actions.ActionTypes;
+using Robust.Shared.GameStates;
 
 namespace Content.Shared.Abilities.Psionics
 {
-    [RegisterComponent]
+    [RegisterComponent, NetworkedComponent]
     public sealed class PsionicComponent : Component
     {
         public ActionType? PsionicAbility = null;
