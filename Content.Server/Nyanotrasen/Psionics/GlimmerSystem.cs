858cc2db51e5be1c36fc02a4c1ed3d2c2f4bfa95
Rane <elijahrane@gmail.com>
2022-09-25 13:34:52 -0400

    clean up grammateus

--- Content.Server/Nyanotrasen/Psionics/GlimmerSystem.cs
+++ Content.Server/Nyanotrasen/Psionics/GlimmerSystem.cs
@@ -1,9 +1,4 @@
 using Content.Shared.Abilities.Psionics;
-using Content.Shared.Radio;
-using Content.Server.Research.SophicScribe;
-using Content.Server.Ghost.Components;
-using Content.Server.Radio.EntitySystems;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 
 namespace Content.Server.Psionics
@@ -11,19 +6,14 @@ namespace Content.Server.Psionics
     public sealed class GlimmerSystem : EntitySystem
     {
         [Dependency] private readonly SharedGlimmerSystem _sharedGlimmerSystem = default!;
-        [Dependency] private readonly RadioSystem _radioSystem = default!;
-        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
         [Dependency] private readonly IRobustRandom _robustRandom = default!;
 
         public float DecayAccumulator = 0;
-        public float GlimmerAnnounceAccumulator = 0;
         public const float SecondsToLoseOneGlimmer = 10f;
-        public const float GlimmerAnnounceIntervalSeconds = 120f;
         public override void Update(float frameTime)
         {
             base.Update(frameTime);
             DecayAccumulator += frameTime;
-            GlimmerAnnounceAccumulator += frameTime;
 
             if (DecayAccumulator > SecondsToLoseOneGlimmer)
             {
@@ -31,22 +21,6 @@ namespace Content.Server.Psionics
                     _sharedGlimmerSystem.AddToGlimmer(-1);
                 DecayAccumulator -= SecondsToLoseOneGlimmer;
             }
-
-            if (GlimmerAnnounceAccumulator > GlimmerAnnounceIntervalSeconds)
-            {
-                GlimmerAnnounceAccumulator -= GlimmerAnnounceIntervalSeconds;
-                if (_sharedGlimmerSystem.Glimmer == 0)
-                    return;
-                foreach (var scribe in EntityQuery<SophicScribeComponent>())
-                {
-                    if (!TryComp<IntrinsicRadioComponent>(scribe.Owner, out var radio)) return;
-
-                    var message = Loc.GetString("glimmer-report", ("level", _sharedGlimmerSystem.Glimmer));
-                    var channel = _prototypeManager.Index<RadioChannelPrototype>("Science");
-                    _radioSystem.SpreadMessage(radio, scribe.Owner, message, channel);
-                }
-            }
         }
-
     }
 }
