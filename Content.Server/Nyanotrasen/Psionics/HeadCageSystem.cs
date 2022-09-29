c082915a3d49b300bab9ef08062946c705f2c453
Rane <elijahrane@gmail.com>
2022-09-29 11:40:36 -0400

    doafter works

--- Content.Server/Nyanotrasen/Psionics/HeadCageSystem.cs
+++ Content.Server/Nyanotrasen/Psionics/HeadCageSystem.cs
@@ -3,6 +3,7 @@ using Content.Shared.Inventory;
 using Content.Shared.Inventory.Events;
 using Content.Shared.Clothing.Components;
 using Content.Shared.Interaction.Components;
+using Content.Shared.Interaction;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Alert;
 using Content.Shared.Abilities.Psionics;
@@ -26,8 +27,11 @@ namespace Content.Server.Psionics
             base.Initialize();
             SubscribeLocalEvent<HeadCageComponent, GotEquippedEvent>(OnCageEquipped);
             SubscribeLocalEvent<HeadCageComponent, GotUnequippedEvent>(OnCageUnequipped);
+            SubscribeLocalEvent<HeadCageComponent, AfterInteractEvent>(OnAfterInteract);
             SubscribeLocalEvent<ResistCageSuccessfulEvent>(OnResistCageSuccessful);
             SubscribeLocalEvent<ResistCageCancelledEvent>(OnResistCageCancelled);
+            SubscribeLocalEvent<PutOnCageCancelledEvent>(OnPutOnCageCancelled);
+            SubscribeLocalEvent<PutOnCageSuccessfulEvent>(OnPutOnCageSuccessful);
         }
 
         private void OnCageEquipped(EntityUid uid, HeadCageComponent component, GotEquippedEvent args)
@@ -52,6 +56,34 @@ namespace Content.Server.Psionics
             component.IsActive = false;
         }
 
+        private void OnAfterInteract(EntityUid uid, HeadCageComponent component, AfterInteractEvent args)
+        {
+            if (!args.CanReach)
+                return;
+
+            if (component.CancelToken != null)
+                return;
+
+            if (args.Target == null)
+                return;
+
+            if (_inventory.TryGetSlotEntity(args.Target.Value, "head", out var _))
+                return;
+
+            component.CancelToken = new CancellationTokenSource();
+
+            _doAfterSystem.DoAfter(new DoAfterEventArgs(args.User, 5, component.CancelToken.Token)
+            {
+                BroadcastFinishedEvent = new PutOnCageSuccessfulEvent(args.Target.Value, uid, args.User),
+                BroadcastCancelledEvent = new PutOnCageCancelledEvent(args.Target.Value, uid, args.User),
+                BreakOnUserMove = true,
+                BreakOnTargetMove = true,
+                BreakOnStun = true,
+                BreakOnDamage = true,
+                NeedHand = true
+            });
+        }
+
         private void OnResistCageCancelled(ResistCageCancelledEvent args)
         {
             if (!EntityManager.TryGetComponent<HeadCageComponent>(args.Cage, out var cageComp))
@@ -72,6 +104,25 @@ namespace Content.Server.Psionics
             RemComp<UnremoveableComponent>(args.Cage);
             if (_inventory.TryUnequip(args.Caged, "head", force: true))
                 _alertsSystem.ClearAlert(args.Caged, AlertType.Caged);
+        }
+
+        private void OnPutOnCageCancelled(PutOnCageCancelledEvent args)
+        {
+            if (!EntityManager.TryGetComponent<HeadCageComponent>(args.Cage, out var cageComp))
+                return;
+
+            cageComp.CancelToken = null;
+        }
+
+        private void OnPutOnCageSuccessful(PutOnCageSuccessfulEvent args)
+        {
+            if (!EntityManager.TryGetComponent<HeadCageComponent>(args.Cage, out var cageComp))
+                return;
+
+            // _audioSystem.PlayPvs(cageComp.EndCageSound, args.Caged);
+            _inventory.TryEquip(args.Cager, args.Caged, args.Cage, "head");
+
+            cageComp.CancelToken = null;
         }
 
         public void ResistCage(EntityUid uid)
@@ -127,5 +178,32 @@ namespace Content.Server.Psionics
                 Cage = cage;
             }
         }
+
+        private sealed class PutOnCageCancelledEvent : EntityEventArgs
+        {
+            public EntityUid Caged;
+            public EntityUid Cage;
+            public EntityUid Cager;
+            public PutOnCageCancelledEvent(EntityUid caged, EntityUid cage, EntityUid cager)
+            {
+                Caged = caged;
+                Cage = cage;
+                Cager = cager;
+            }
+        }
+
+        private sealed class PutOnCageSuccessfulEvent : EntityEventArgs
+        {
+            public EntityUid Caged;
+            public EntityUid Cage;
+            public EntityUid Cager;
+            public PutOnCageSuccessfulEvent(EntityUid caged, EntityUid cage, EntityUid cager)
+            {
+                Caged = caged;
+                Cage = cage;
+                Cager = cager;
+            }
+        }
+
     }
 }
