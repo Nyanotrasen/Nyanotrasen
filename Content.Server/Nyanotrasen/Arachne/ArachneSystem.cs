627358e7877077eb91e5dd47665a8bbec01e0faa
Rane <elijahrane@gmail.com>
2022-10-26 20:11:51 -0400

    needs more polish HOWEVER

--- Content.Server/Nyanotrasen/Arachne/ArachneSystem.cs
+++ Content.Server/Nyanotrasen/Arachne/ArachneSystem.cs
@@ -1,12 +1,15 @@
 using Content.Shared.Arachne;
 using Content.Shared.Actions;
 using Content.Shared.Actions.ActionTypes;
+using Content.Shared.IdentityManagement;
 using Content.Server.Coordinates.Helpers;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Timing;
+using Content.Server.Nutrition.EntitySystems;
+using Content.Server.Nutrition.Components;
+using Content.Server.Popups;
 using Robust.Server.GameObjects;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Player;
-
+using Robust.Shared.Random;
 
 namespace Content.Server.Arachne
 {
@@ -14,6 +17,10 @@ namespace Content.Server.Arachne
     {
         [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
         [Dependency] private readonly SharedActionsSystem _actions = default!;
+        [Dependency] private readonly ThirstSystem _thirstSystem = default!;
+        [Dependency] private readonly PopupSystem _popupSystem = default!;
+        [Dependency] private readonly IRobustRandom _random = default!;
+
 
         public override void Initialize()
         {
@@ -30,7 +37,28 @@ namespace Content.Server.Arachne
 
         private void OnSpinWeb(SpinWebActionEvent args)
         {
+            TryComp<HungerComponent>(args.Performer, out var hunger);
+            TryComp<ThirstComponent>(args.Performer, out var thirst);
+
+            if (hunger != null && thirst != null)
+            {
+                if (hunger.CurrentHungerThreshold <= Shared.Nutrition.Components.HungerThreshold.Peckish)
+                {
+                    _popupSystem.PopupEntity(Loc.GetString("spin-web-action-hungry"), args.Performer, Filter.Entities(args.Performer), Shared.Popups.PopupType.MediumCaution);
+                    return;
+                }
+                if (thirst.CurrentThirstThreshold <= ThirstThreshold.Thirsty)
+                {
+                    _popupSystem.PopupEntity(Loc.GetString("spin-web-action-thirsty"), args.Performer, Filter.Entities(args.Performer), Shared.Popups.PopupType.MediumCaution);
+                    return;
+                }
+                hunger.UpdateFood(-8);
+                _thirstSystem.UpdateThirst(thirst, -20);
+            }
+
             Spawn("ArachneWeb", Transform(args.Performer).Coordinates.SnapToGrid());
+            _popupSystem.PopupEntity(Loc.GetString("spun-web-third-person", ("spider", Identity.Entity(args.Performer, EntityManager))), args.Performer, Filter.PvsExcept(args.Performer), Shared.Popups.PopupType.MediumCaution);
+            _popupSystem.PopupEntity(Loc.GetString("spun-web-second-person"), args.Performer, Filter.Entities(args.Performer), Shared.Popups.PopupType.Medium);
             args.Handled = true;
         }
     }
--- Resources/Locale/en-US/abilities/arachne.ftl
+++ Resources/Locale/en-US/abilities/arachne.ftl
@@ -1,2 +1,6 @@
 action-name-spin-web = Spin Web
 action-desc-spin-web = Use your spinnerets to make a spider web in the current tile. Makes you hungrier and thirstier.
+spin-web-action-hungry = You're too hungry to spin a web!
+spin-web-action-thirsty = You're too thirsty to spin a web!
+spun-web-second-person = You spin up a web.
+spun-web-third-person = {CAPITALIZE(THE($spider))} spins up a web!
