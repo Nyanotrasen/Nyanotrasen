12d19db4d30bd9b0d7a37d429d323c00da98acc1
Rane <elijahrane@gmail.com>
2022-10-27 20:31:01 -0400

    doafters complete

--- Content.Server/Nyanotrasen/Arachne/ArachneSystem.cs
+++ Content.Server/Nyanotrasen/Arachne/ArachneSystem.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using Content.Shared.Arachne;
 using Content.Shared.Actions;
 using Content.Shared.Actions.ActionTypes;
@@ -10,15 +11,14 @@ using Content.Server.Nutrition.EntitySystems;
 using Content.Server.Nutrition.Components;
 using Content.Server.Popups;
 using Content.Server.Buckle.Components;
+using Content.Server.DoAfter;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Player;
 using Robust.Shared.Physics;
-using Robust.Shared.Random;
 using Robust.Shared.Containers;
 using Robust.Shared.Map;
 using Content.Shared.Physics;
 using Content.Shared.Doors.Components;
-using Content.Shared.MobState.Components;
 
 namespace Content.Server.Arachne
 {
@@ -28,10 +28,8 @@ namespace Content.Server.Arachne
         [Dependency] private readonly SharedActionsSystem _actions = default!;
         [Dependency] private readonly ThirstSystem _thirstSystem = default!;
         [Dependency] private readonly PopupSystem _popupSystem = default!;
-        [Dependency] private readonly IRobustRandom _random = default!;
         [Dependency] private readonly IMapManager _mapManager = default!;
-        [Dependency] private readonly ITileDefinitionManager _tileDefMan = default!;
-
+        [Dependency] private readonly DoAfterSystem _doAfter = default!;
 
         public override void Initialize()
         {
@@ -41,6 +39,8 @@ namespace Content.Server.Arachne
             SubscribeLocalEvent<WebComponent, GetVerbsEvent<AlternativeVerb>>(AddRestVerb);
             SubscribeLocalEvent<WebComponent, BuckleChangeEvent>(OnBuckleChange);
             SubscribeLocalEvent<SpinWebActionEvent>(OnSpinWeb);
+            SubscribeLocalEvent<WebSuccessfulEvent>(OnWebSuccessful);
+            SubscribeLocalEvent<WebCancelledEvent>(OnWebCancelled);
         }
 
         private void OnInit(EntityUid uid, ArachneComponent component, ComponentInit args)
@@ -104,6 +104,9 @@ namespace Content.Server.Arachne
 
         private void OnSpinWeb(SpinWebActionEvent args)
         {
+            if (!TryComp<ArachneComponent>(args.Performer, out var arachne) || arachne.CancelToken != null)
+                return;
+
             TryComp<HungerComponent>(args.Performer, out var hunger);
             TryComp<ThirstComponent>(args.Performer, out var thirst);
 
@@ -140,13 +143,58 @@ namespace Content.Server.Arachne
                 }
             }
 
-            hunger?.UpdateFood(-8);
-            if (thirst != null)
+            _popupSystem.PopupEntity(Loc.GetString("spin-web-start-third-person", ("spider", Identity.Entity(args.Performer, EntityManager))), args.Performer, Filter.PvsExcept(args.Performer), Shared.Popups.PopupType.MediumCaution);
+            _popupSystem.PopupEntity(Loc.GetString("spin-web-start-second-person"), args.Performer, Filter.Entities(args.Performer), Shared.Popups.PopupType.Medium);
+            arachne.CancelToken = new CancellationTokenSource();
+            _doAfter.DoAfter(new DoAfterEventArgs(args.Performer, arachne.WebDelay, arachne.CancelToken.Token)
+            {
+                BroadcastFinishedEvent = new WebSuccessfulEvent(args.Performer),
+                BroadcastCancelledEvent = new WebCancelledEvent(args.Performer),
+                BreakOnUserMove = true,
+                BreakOnStun = true,
+            });
+        }
+
+        private void OnWebCancelled(WebCancelledEvent ev)
+        {
+            if (!EntityManager.TryGetComponent(ev.Webber, out ArachneComponent? arachne))
+                return;
+            arachne.CancelToken = null;
+        }
+
+        private void OnWebSuccessful(WebSuccessfulEvent ev)
+        {
+            if (!EntityManager.TryGetComponent(ev.Webber, out ArachneComponent? arachne))
+                return;
+
+            arachne.CancelToken = null;
+
+            if (TryComp<HungerComponent>(ev.Webber, out var hunger))
+                hunger.UpdateFood(-8);
+            if (TryComp<ThirstComponent>(ev.Webber, out var thirst))
                 _thirstSystem.UpdateThirst(thirst, -20);
-            Spawn("ArachneWeb", coords);
-            _popupSystem.PopupEntity(Loc.GetString("spun-web-third-person", ("spider", Identity.Entity(args.Performer, EntityManager))), args.Performer, Filter.PvsExcept(args.Performer), Shared.Popups.PopupType.MediumCaution);
-            _popupSystem.PopupEntity(Loc.GetString("spun-web-second-person"), args.Performer, Filter.Entities(args.Performer), Shared.Popups.PopupType.Medium);
-            args.Handled = true;
+            Spawn("ArachneWeb", Transform(ev.Webber).Coordinates.SnapToGrid());
+            _popupSystem.PopupEntity(Loc.GetString("spun-web-third-person", ("spider", Identity.Entity(ev.Webber, EntityManager))), ev.Webber, Filter.PvsExcept(ev.Webber), Shared.Popups.PopupType.MediumCaution);
+            _popupSystem.PopupEntity(Loc.GetString("spun-web-second-person"), ev.Webber, Filter.Entities(ev.Webber), Shared.Popups.PopupType.Medium);
+        }
+
+        private sealed class WebCancelledEvent : EntityEventArgs
+        {
+            public EntityUid Webber;
+
+            public WebCancelledEvent(EntityUid webber)
+            {
+                Webber = webber;
+            }
+        }
+
+        private sealed class WebSuccessfulEvent : EntityEventArgs
+        {
+            public EntityUid Webber;
+            public WebSuccessfulEvent(EntityUid webber)
+            {
+                Webber = webber;
+            }
         }
     }
     public sealed class SpinWebActionEvent : InstantActionEvent {}
