ff764171fd0ffc2fb53eab1e838f3a4315743096
Rane <elijahrane@gmail.com>
2022-11-28 19:18:07 -0500

    commands for reading, adding, clearing laws

--- Content.Server/Nyanotrasen/Borgs/LawCommands.cs
+++ Content.Server/Nyanotrasen/Borgs/LawCommands.cs
@@ -19,18 +19,21 @@ public sealed class ListLawsCommand : IConsoleCommand
     {
         var entityManager = IoCManager.Resolve<IEntityManager>();
         var player = shell.Player as IPlayerSession;
-        IPlayerData? data;
+        EntityUid? entity = null;
+
         if (args.Length == 0 && player != null)
         {
-            data = player.Data;
+            entity = player.ContentData()?.Mind?.CurrentEntity;
         }
-        else if (player == null || !IoCManager.Resolve<IPlayerManager>().TryGetPlayerDataByUsername(args[0], out data))
+        else if (IoCManager.Resolve<IPlayerManager>().TryGetPlayerDataByUsername(args[0], out var data))
         {
-            shell.WriteLine("Can't find the playerdata.");
-            return;
+            entity = data.ContentData()?.Mind?.CurrentEntity;
+        }
+        else if (EntityUid.TryParse(args[0], out var foundEntity))
+        {
+            entity = foundEntity;
         }
 
-        var entity = data.ContentData()?.Mind?.CurrentEntity;
         if (entity == null)
         {
             shell.WriteLine("Can't find entity.");
@@ -43,10 +46,89 @@ public sealed class ListLawsCommand : IConsoleCommand
             return;
         }
 
-        shell.WriteLine($"Laws for entity {entity}:");
+        shell.WriteLine($"Laws for {entityManager.ToPrettyString(entity.Value)}:");
         foreach (var law in laws.Laws)
         {
             shell.WriteLine(law);
         }
+    }
+}
+
+[AdminCommand(AdminFlags.Fun)]
+public sealed class ClearLawsCommand : IConsoleCommand
+{
+    public string Command => "lawclear";
+    public string Description => Loc.GetString("command-lawclear-description");
+    public string Help => Loc.GetString("command-lawclear-help");
+    public async void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var entityManager = IoCManager.Resolve<IEntityManager>();
+        var player = shell.Player as IPlayerSession;
+        EntityUid? entity = null;
+
+        if (args.Length == 0 && player != null)
+        {
+            entity = player.ContentData()?.Mind?.CurrentEntity;
+        }
+        else if (IoCManager.Resolve<IPlayerManager>().TryGetPlayerDataByUsername(args[0], out var data))
+        {
+            entity = data.ContentData()?.Mind?.CurrentEntity;
+        }
+        else if (EntityUid.TryParse(args[0], out var foundEntity))
+        {
+            entity = foundEntity;
+        }
+
+        if (entity == null)
+        {
+            shell.WriteLine("Can't find entity.");
+            return;
+        }
+
+        if (!entityManager.TryGetComponent<LawsComponent>(entity.Value, out var laws))
+        {
+            shell.WriteLine("Entity has no laws component to clear");
+            return;
+        }
+
+        entityManager.EntitySysManager.GetEntitySystem<LawsSystem>().ClearLaws(entity.Value, laws);
+    }
+}
+
+[AdminCommand(AdminFlags.Fun)]
+public sealed class AddLawCommand : IConsoleCommand
+{
+    public string Command => "lawadd";
+    public string Description => Loc.GetString("command-lawadd-description");
+    public string Help => Loc.GetString("command-lawadd-help");
+    public async void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var entityManager = IoCManager.Resolve<IEntityManager>();
+        var player = shell.Player as IPlayerSession;
+        EntityUid? entity = null;
+
+        if (args.Length != 2)
+        {
+            shell.WriteLine("Wrong number of arguments.");
+            return;
+        }
+
+        if (IoCManager.Resolve<IPlayerManager>().TryGetPlayerDataByUsername(args[0], out var data))
+        {
+            entity = data.ContentData()?.Mind?.CurrentEntity;
+        }
+        else if (EntityUid.TryParse(args[0], out var foundEntity))
+        {
+            entity = foundEntity;
+        }
+
+        if (entity == null)
+        {
+            shell.WriteLine("Can't find entity.");
+            return;
+        }
+
+        var laws = entityManager.EnsureComponent<LawsComponent>(entity.Value);
+        entityManager.EntitySysManager.GetEntitySystem<LawsSystem>().AddLaw(entity.Value, args[1], laws);
     }
 }
