2c26eddc3ffecf9b7090a6489451bda9e66739d7
Moony <moony@hellomouse.net>
2022-12-21 18:40:10 -0600

    Separates Nyanotrasen's ui.layout CVar from everyone else's.
    And changes the default to Separated.

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -1131,7 +1131,7 @@ namespace Content.Shared.CCVar
          */
 
         public static readonly CVarDef<string> UILayout =
-            CVarDef.Create("ui.layout", "Default", CVar.CLIENTONLY | CVar.ARCHIVE);
+            CVarDef.Create("nyanotrasen.ui.layout", "Separated", CVar.CLIENTONLY | CVar.ARCHIVE);
 
 
 
