5852af90cac111af641b59dab550e3541a935983
Rane <elijahrane@gmail.com>
2023-01-17 22:03:40 -0500

    optimizations

--- Content.Client/SimpleStation14/Overlays/Systems/HealthGlassesSystem.cs
+++ Content.Client/SimpleStation14/Overlays/Systems/HealthGlassesSystem.cs
@@ -1,33 +1,51 @@
 using Content.Client.HealthOverlay;
-using Content.Shared.Inventory.Events;
 using Content.Shared.SimpleStation14.Clothing;
+using Content.Shared.GameTicking;
+using Robust.Client.Player;
+using Robust.Client.GameObjects;
 
 namespace Content.Client.SimpleStation14.Overlays
 {
     public sealed class HealthGlassesSystem : EntitySystem
     {
+        [Dependency] private readonly HealthOverlaySystem _healthOverlay = default!;
+        [Dependency] private readonly IPlayerManager _player = default!;
         public override void Initialize()
         {
             base.Initialize();
 
+            SubscribeLocalEvent<HealthGlassesComponent, ComponentInit>(OnInit);
             SubscribeLocalEvent<HealthGlassesComponent, ComponentRemove>(OnRemove);
+            SubscribeLocalEvent<HealthGlassesComponent, PlayerAttachedEvent>(OnPlayerAttached);
+            SubscribeLocalEvent<HealthGlassesComponent, PlayerDetachedEvent>(OnPlayerDetached);
+            SubscribeLocalEvent<RoundRestartCleanupEvent>(OnRoundRestart);
         }
 
-        public override void Update(float frameTime)
+        private void OnInit(EntityUid uid, HealthGlassesComponent component, ComponentInit args)
         {
-            base.Update(frameTime);
+            if (_player.LocalPlayer?.ControlledEntity == uid)
+                _healthOverlay.Enabled = true;
 
-            foreach (var glassed in EntityQuery<HealthGlassesComponent>())
-            {
-                var system = EntitySystem.Get<HealthOverlaySystem>();
-                if (!system.Enabled) system.Enabled = true;
-            }
         }
-
         private void OnRemove(EntityUid uid, HealthGlassesComponent component, ComponentRemove args)
         {
-            var system = EntitySystem.Get<HealthOverlaySystem>();
-            system.Enabled = false;
+            if (_player.LocalPlayer?.ControlledEntity == uid)
+                _healthOverlay.Enabled = false;
+        }
+
+        private void OnPlayerAttached(EntityUid uid, HealthGlassesComponent component, PlayerAttachedEvent args)
+        {
+            _healthOverlay.Enabled = true;
+        }
+
+        private void OnPlayerDetached(EntityUid uid, HealthGlassesComponent component, PlayerDetachedEvent args)
+        {
+            _healthOverlay.Enabled = false;
+        }
+
+        private void OnRoundRestart(RoundRestartCleanupEvent args)
+        {
+            _healthOverlay.Enabled = false;
         }
     }
 }
