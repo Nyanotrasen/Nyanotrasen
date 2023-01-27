827ecf56d67c539963fb792a21983937a73bf577
Rane <elijahrane@gmail.com>
2023-01-26 19:22:48 -0500

    it just works

--- Content.Client/EntityHealthBar/EntityHealthBarOverlay.cs
+++ Content.Client/EntityHealthBar/EntityHealthBarOverlay.cs
@@ -1,5 +1,8 @@
 using Content.Shared.Damage;
+using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.FixedPoint;
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
 using Robust.Shared.Enums;
@@ -12,6 +15,8 @@ public sealed class EntityHealthBarOverlay : Overlay
 {
     private readonly IEntityManager _entManager;
     private readonly SharedTransformSystem _transform;
+    private readonly MobStateSystem _mobStateSystem;
+    private readonly MobThresholdSystem _mobThresholdSystem;
     private readonly Texture _barTexture;
     private readonly ShaderInstance _shader;
     public override OverlaySpace Space => OverlaySpace.WorldSpaceBelowFOV;
@@ -21,6 +26,9 @@ public sealed class EntityHealthBarOverlay : Overlay
     {
         _entManager = entManager;
         _transform = _entManager.EntitySysManager.GetEntitySystem<SharedTransformSystem>();
+        _mobStateSystem = _entManager.EntitySysManager.GetEntitySystem<MobStateSystem>();
+        _mobThresholdSystem = _entManager.EntitySysManager.GetEntitySystem<MobThresholdSystem>();
+
         var sprite = new SpriteSpecifier.Rsi(new ResourcePath("/Textures/Interface/Misc/progress_bar.rsi"), "icon");
         _barTexture = _entManager.EntitySysManager.GetEntitySystem<SpriteSystem>().Frame0(sprite);
 
@@ -47,6 +55,9 @@ public sealed class EntityHealthBarOverlay : Overlay
                 continue;
             }
 
+            if (DamageContainer != null && dmg.DamageContainerID != DamageContainer)
+                continue;
+
             var worldPosition = _transform.GetWorldPosition(xform);
             var worldMatrix = Matrix3.CreateTranslation(worldPosition);
 
@@ -54,49 +65,93 @@ public sealed class EntityHealthBarOverlay : Overlay
             Matrix3.Multiply(rotationMatrix, scaledWorld, out var matty);
 
             handle.SetTransform(matty);
+
             // Use the sprite itself if we know its bounds. This means short or tall sprites don't get overlapped
             // by the bar.
             float yOffset;
             if (spriteQuery.TryGetComponent(mob.Owner, out var sprite))
             {
-                yOffset = sprite.Bounds.Height / 2f + 0.05f;
+                yOffset = sprite.Bounds.Height + 15f;
             }
             else
             {
-                yOffset = 0.5f;
+                yOffset = 1f;
             }
 
             // Position above the entity (we've already applied the matrix transform to the entity itself)
             // Offset by the texture size for every do_after we have.
             var position = new Vector2(-_barTexture.Width / 2f / EyeManager.PixelsPerMeter,
-                yOffset / scale / EyeManager.PixelsPerMeter * scale);
+                yOffset / EyeManager.PixelsPerMeter);
 
             // Draw the underlying bar texture
             handle.DrawTexture(_barTexture, position);
+
+            // we are all progressing towards death every day
+            (float ratio, bool inCrit) deathProgress = CalcProgress(mob.Owner, mob, dmg);
+
+            var color = GetProgressColor(deathProgress.ratio, deathProgress.inCrit);
 
             // Hardcoded width of the progress bar because it doesn't match the texture.
             const float startX = 2f;
             const float endX = 22f;
 
-            var xProgress = (endX - startX) * 1 + startX;
+            var xProgress = (endX - startX) * deathProgress.ratio + startX;
 
             var box = new Box2(new Vector2(startX, 3f) / EyeManager.PixelsPerMeter, new Vector2(xProgress, 4f) / EyeManager.PixelsPerMeter);
             box = box.Translated(position);
-            handle.DrawRect(box, Color.Green);
+            handle.DrawRect(box, color);
         }
 
         handle.UseShader(null);
         handle.SetTransform(Matrix3.Identity);
     }
 
-    public static Color GetProgressColor(float progress)
+    /// <summary>
+    /// Returns a ratio between 0 and 1, and whether the entity is in crit.
+    /// </summary>
+    private (float, bool) CalcProgress(EntityUid uid, MobStateComponent component, DamageableComponent dmg)
+    {
+        if (_mobStateSystem.IsAlive(uid, component))
+        {
+            if (!_mobThresholdSystem.TryGetThresholdForState(uid, MobState.Critical, out var threshold))
+                return (1, false);
+
+            var ratio = 1 - ((FixedPoint2)(dmg.TotalDamage / threshold)).Float();
+            return (ratio, false);
+        }
+
+        if (_mobStateSystem.IsCritical(uid, component))
+        {
+            if (!_mobThresholdSystem.TryGetThresholdForState(uid, MobState.Critical, out var critThreshold) ||
+                !_mobThresholdSystem.TryGetThresholdForState(uid, MobState.Dead, out var deadThreshold))
+            {
+                return (1, true);
+            }
+
+            var ratio = 1 -
+                    ((dmg.TotalDamage - critThreshold) /
+                    (deadThreshold - critThreshold)).Value.Float();
+
+            return (ratio, true);
+        }
+
+        return (0, true);
+    }
+
+    public static Color GetProgressColor(float progress, bool crit)
     {
         if (progress >= 1.0f)
         {
             return new Color(0f, 1f, 0f);
         }
         // lerp
-        var hue = (5f / 18f) * progress;
-        return Color.FromHsv((hue, 1f, 0.75f, 1f));
+        if (!crit)
+        {
+            var hue = (5f / 18f) * progress;
+            return Color.FromHsv((hue, 1f, 0.75f, 1f));
+        } else
+        {
+            return Color.Red;
+        }
     }
 }
