cf0813e8436c99c91b96b763f994489ac2f37588
Rane <elijahrane@gmail.com>
2023-02-15 19:11:25 -0500

    Cleanbot rewrite + medibot/mouse minor fixes

--- Content.Server/NPC/HTN/PrimitiveTasks/Operators/Specific/CleanBotCleanOperator.cs
+++ Content.Server/NPC/HTN/PrimitiveTasks/Operators/Specific/CleanBotCleanOperator.cs
@@ -2,43 +2,43 @@ using Content.Server.Silicons.Bots;
 
 namespace Content.Server.NPC.HTN.PrimitiveTasks.Operators.Specific;
 
-public sealed class MedibotInjectOperator : HTNOperator
+public sealed class CleanBotCleanOperator : HTNOperator
 {
     [Dependency] private readonly IEntityManager _entManager = default!;
-    private MedibotSystem _medibotSystem = default!;
+    private CleanBotSystem _cleanbotSystem = default!;
 
-    [DataField("injectKey")]
-    public string InjectKey = string.Empty;
+    [DataField("cleanKey")]
+    public string CleanKey = string.Empty;
 
     public override void Initialize(IEntitySystemManager sysManager)
     {
         base.Initialize(sysManager);
-        _medibotSystem = sysManager.GetEntitySystem<MedibotSystem>();
+        _cleanbotSystem = sysManager.GetEntitySystem<CleanBotSystem>();
     }
 
     public override HTNOperatorStatus Update(NPCBlackboard blackboard, float frameTime)
     {
         var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
-        var target = blackboard.GetValue<EntityUid>(InjectKey);
+        var target = blackboard.GetValue<EntityUid>(CleanKey);
 
         if (!target.IsValid() || _entManager.Deleted(target))
             return HTNOperatorStatus.Failed;
 
-        if (!_entManager.TryGetComponent<MedibotComponent>(owner, out var medibot))
+        if (!_entManager.TryGetComponent<CleanBotComponent>(owner, out var cleanbot))
             return HTNOperatorStatus.Failed;
 
-        if (medibot.CancelToken != null)
+        if (cleanbot.CancelToken != null)
             return HTNOperatorStatus.Continuing;
 
-        if (medibot.InjectTarget == null)
+        if (cleanbot.CleanTarget == null)
         {
-            if (_medibotSystem.NPCStartInject(owner, target, medibot))
+            if (_cleanbotSystem.TryStartClean(owner, cleanbot, target))
                 return HTNOperatorStatus.Continuing;
             else
                 return HTNOperatorStatus.Failed;
         }
 
-        medibot.InjectTarget = null;
+        cleanbot.CleanTarget = null;
 
         return HTNOperatorStatus.Finished;
     }
--- Resources/Locale/en-US/ghost/roles/ghost-role-component.ftl
+++ Resources/Locale/en-US/ghost/roles/ghost-role-component.ftl
@@ -25,3 +25,6 @@ ghost-role-information-cancer-mouse-description = Make off color comments, but n
 
 ghost-role-information-medibot-name = Medibot
 ghost-role-information-medibot-description = Click people to heal them. Heal anyone your autoinjector lets you.
+
+ghost-role-information-cleanbot-name = CleanBot
+ghost-role-information-cleanbot-description = Click puddles to mop them up.
