479894ecdab82affdaaebbda6ec024ba638b4f66
Vordenburg <114301317+Vordenburg@users.noreply.github.com>
2023-03-05 13:43:48 -0500

    Make theft objectives require escaping.

---
+++ Content.Server/Objectives/Requirements/AdditionalObjectiveRequirement.cs
@@ -0,0 +1,27 @@
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Content.Shared.Roles;
+using Content.Server.Objectives.Interfaces;
+
+namespace Content.Server.Objectives.Requirements
+{
+    [DataDefinition]
+    public sealed class AdditionalObjectiveRequirement : IObjectiveRequirement
+    {
+        [DataField("objective", required: true, customTypeSerializer:typeof(PrototypeIdSerializer<JobPrototype>))]
+        private string _objectiveId = default!;
+
+        /// <summary>
+        /// This requirement is met if the additional objective can be added successfully or is already assigned.
+        /// </summary>
+        public bool CanBeAssigned(Mind.Mind mind)
+        {
+            foreach (var assignedObjective in mind.AllObjectives)
+                if (assignedObjective.Prototype.ID == _objectiveId)
+                    return true;
+
+            var objective = IoCManager.Resolve<IPrototypeManager>().Index<ObjectivePrototype>(_objectiveId);
+            return mind.TryAddObjective(objective);
+        }
+    }
+}
