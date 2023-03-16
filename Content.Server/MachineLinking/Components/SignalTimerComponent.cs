c2903738fd66d612ab1e9fce45d2b22ad07e7a79
Finket <va.ketola@gmail.com>
2023-03-16 04:10:30 +0200

    Add Rudimentary Functioning Timers

---
+++ Content.Server/MachineLinking/Components/SignalTimerComponent.cs
@@ -0,0 +1,38 @@
+using Content.Shared.MachineLinking;
+using Robust.Shared.Audio;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server.MachineLinking.Components
+{
+    /// <summary>
+    ///     This is a modified version of the switch which acts as a timer.
+    /// </summary>
+
+    [RegisterComponent]
+    public sealed class SignalTimerComponent : Component
+    {
+        [DataField("length")]
+        public float Length { get; set; } = 20f;
+
+        /// <summary>
+        ///     The port that gets signaled when the switch turns on.
+        /// </summary>
+        [DataField("onPort", customTypeSerializer: typeof(PrototypeIdSerializer<TransmitterPortPrototype>))]
+        public string OnPort = "On";
+
+        /// <summary>
+        ///     The port that gets signaled when the switch turns off.
+        /// </summary>
+        [DataField("offPort", customTypeSerializer: typeof(PrototypeIdSerializer<TransmitterPortPrototype>))]
+        public string OffPort = "Off";
+
+        [DataField("state")]
+        public bool State;
+
+        [DataField("clickSound")]
+        public SoundSpecifier ClickSound { get; set; } = new SoundPathSpecifier("/Audio/Machines/lightswitch.ogg");
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        public float TimeRemaining;
+    }
+}
---
+++ Content.Server/MachineLinking/System/SignalTimerSystem.cs
@@ -0,0 +1,73 @@
+using Content.Server.MachineLinking.Components;
+using Content.Shared.Audio;
+using Content.Shared.Interaction;
+using Robust.Shared.Audio;
+using Robust.Shared.Player;
+
+/// <remarks>
+///     Changes that are still missing:
+///     - Pressing the timer opens its UI instead of just activating it
+///     - UI needs an entry field for Length and a cancel button?
+///     - Ability to change component.Length from within the UI.
+/// </remarks>
+
+namespace Content.Server.MachineLinking.System
+{
+    public sealed class SignalTimerSystem : EntitySystem
+    {
+        [Dependency] private readonly SignalLinkerSystem _signalSystem = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<SignalTimerComponent, ComponentInit>(OnInit);
+            SubscribeLocalEvent<SignalTimerComponent, ActivateInWorldEvent>(OnActivated);
+        }
+
+        private void OnInit(EntityUid uid, SignalTimerComponent component, ComponentInit args)
+        {
+            _signalSystem.EnsureTransmitterPorts(uid, component.OnPort, component.OffPort);
+        }
+
+        private void OnActivated(EntityUid uid, SignalTimerComponent component, ActivateInWorldEvent args)
+        {
+            if (args.Handled)
+                return;
+
+            // do nothing if the timer is already on, for now. Should actually open the UI.
+            // in a separate function the timer will tick down and then reset itself and the component.State.
+            if (component.State)
+                return;
+
+            component.State = !component.State;
+            _signalSystem.InvokePort(uid, component.State ? component.OnPort : component.OffPort);
+            SoundSystem.Play(component.ClickSound.GetSound(), Filter.Pvs(component.Owner), component.Owner,
+                AudioHelpers.WithVariation(0.125f).WithVolume(8f));
+
+            args.Handled = true;
+        }
+
+        // borrowed from MicrowaveSystem
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+
+            foreach (var component in EntityManager.EntityQuery<SignalTimerComponent>())
+            {
+                if (component.State)
+                {
+                    // check if there's still time left
+                    component.TimeRemaining -= frameTime;
+                    if (component.TimeRemaining > 0)
+                        continue;
+
+                    // open door and reset state, as the timer is done
+                    component.State = !component.State;
+                    _signalSystem.InvokePort(component.Owner, component.State ? component.OnPort : component.OffPort);
+                    component.TimeRemaining = component.Length;
+                }
+            }
+        }
+    }
+}
