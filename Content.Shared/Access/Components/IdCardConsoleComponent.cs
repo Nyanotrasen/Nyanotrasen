44da37e1779ed9d161f9dcb867019b72ccb5363b
Vordenburg <114301317+Vordenburg@users.noreply.github.com>
2023-04-16 16:30:04 -0400

    Re-add Mail to IdCardConsoleComponent AccessLevels.

--- Content.Shared/Access/Components/IdCardConsoleComponent.cs
+++ Content.Shared/Access/Components/IdCardConsoleComponent.cs
@@ -51,6 +51,7 @@ public sealed class IdCardConsoleComponent : Component
         "Detective",
         "Captain",
         "Cargo",
+        "Mail",
         "Chapel",
         "Chemistry",
         "ChiefEngineer",
