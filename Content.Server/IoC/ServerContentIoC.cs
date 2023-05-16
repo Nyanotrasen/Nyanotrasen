a190fc82fad40ce2975980282f04d297c2d38789
Moony <moony@hellomouse.net>
2023-05-16 06:36:45 -0500

    Dynamic space world generation and debris. (#15120)
    * World generation (squash)
    
    * Test fixes.
    
    * command
    
    * o
    
    * Access cleanup.
    
    * Documentation touchups.
    
    * Use a prototype serializer for BiomeSelectionComponent
    
    * Struct enumerator in SimpleFloorPlanPopulatorSystem
    
    * Safety margins around PoissonDiskSampler, cookie acquisition methodologies
    
    * Struct enumerating PoissonDiskSampler; internal side
    
    * Struct enumerating PoissonDiskSampler: Finish it
    
    * Update WorldgenConfigSystem.cs
    
    awa
    
    ---------
    
    Co-authored-by: moonheart08 <moonheart08@users.noreply.github.com>
    Co-authored-by: 20kdc <asdd2808@gmail.com>

--- Content.Server/IoC/ServerContentIoC.cs
+++ Content.Server/IoC/ServerContentIoC.cs
@@ -20,6 +20,7 @@ using Content.Server.Preferences.Managers;
 using Content.Server.ServerInfo;
 using Content.Server.ServerUpdates;
 using Content.Server.Voting.Managers;
+using Content.Server.Worldgen.Tools;
 using Content.Shared.Administration;
 using Content.Shared.Administration.Logs;
 using Content.Shared.Administration.Managers;
@@ -60,6 +61,7 @@ namespace Content.Server.IoC
             IoCManager.Register<RedialManager>();
             IoCManager.Register<UserDbDataManager>();
             IoCManager.Register<ServerInfoManager>();
+            IoCManager.Register<PoissonDiskSampler>();
         }
     }
 }
