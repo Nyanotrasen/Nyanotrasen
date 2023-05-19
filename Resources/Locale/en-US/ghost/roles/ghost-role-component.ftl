RebelledCombine/Zona-14

[Locale] Ghost roles are fluent (#16229)

Resources/Locale/en-US/ghost/roles/ghost-role-component.ftl
@@ -20,3 +20,121 @@ ghost-role-information-hamlet-description = Lives in the station bridge, has a b
 ghost-role-information-slimes-name = Slime
 ghost-role-information-slimes-description = An ordinary slime with no special needs or interests. He just lives.
 ghost-role-information-angry-slimes-description = Everything around you irritates your instincts, destroy them!
+
+ghost-role-information-xeno-name = Xeno
+ghost-role-information-xeno-description = You are a xeno, co-operate with your hive to kill all crewmembers!
+ghost-role-information-xeno-rules = You are an antagonist, smack, slash, and wack!
+
+ghost-role-information-revenant-name = Revenant
+ghost-role-information-revenant-description = You are a Revenant. Use your powers to harvest souls and unleash chaos upon the crew. Unlock new abilities with the essence you harvest.
+ghost-role-information-revenant-rules = You are an antagonist, harvest, defile, and drive the crew insane.
+
+ghost-role-information-kangaroo-name = Kangaroo
+ghost-role-information-kangaroo-description = You're a kangaroo! Do whatever kangaroos do.
+
+ghost-role-information-monkey-name = Monkey
+ghost-role-information-monkey-description = Ook ook!
+
+ghost-role-information-rat-king-name = Rat King
+ghost-role-information-rat-king-description = You are the Rat King, scavenge food in order to produce rat minions to do your bidding.
+ghost-role-information-rat-king-rules = You are an antagonist, scavenge, attack, and grow your hoard!
+
+ghost-role-information-rat-servant-name = Rat Servant
+ghost-role-information-rat-servant-description = You are a Rat Servant. You must follow your king's orders.
+ghost-role-information-rat-servant-rules = You are an antagonist, scavenge, attack, and serve your king!
+
+ghost-role-information-salvage-carp-name = Space carp on salvage wreck
+ghost-role-information-salvage-carp-description = Defend the loot inside the salvage wreck!
+
+ghost-role-information-sentient-carp-name = Sentient Carp
+ghost-role-information-sentient-carp-description = Help the dragon flood the station with carps!
+
+ghost-role-information-willow-name = Willow the kangaroo
+ghost-role-information-willow-description = You're a kangaroo named willow! willow likes to box.
+
+ghost-role-information-space-tick-name = Space tick
+ghost-role-information-space-tick-description = Wreak havoc on the station!
+
+ghost-role-information-salvage-tick-name = Space tick on salvage wreck
+ghost-role-information-salvage-tick-description = Defend the loot inside the salvage wreck!
+
+ghost-role-information-honkbot-name = Honkbot
+ghost-role-information-honkbot-description = An artificial being of pure evil.
+
+ghost-role-information-jonkbot-name = Jonkbot
+ghost-role-information-jonkbot-description = An artificial being of pure evil.
+
+ghost-role-information-mimebot-name = Mimebot
+ghost-role-information-mimebot-description = A Mimebot, act like a mime but dont act like a greytider.
+
+ghost-role-information-space-bear-name = Space bear
+ghost-role-information-space-bear-description = You're a bear! Do bear things.
+
+ghost-role-information-salvage-bear-name = Space bear on salvage wreck
+ghost-role-information-salvage-bear-description = Defend the loot inside the salvage wreck!
+
+ghost-role-information-space-kangaroo-name = Space kangaroo
+ghost-role-information-space-kangaroo-description = You're a kangaroo! Do kangaroo things.
+
+ghost-role-information-salvage-kangaroo-name = Space kangaroo on salvage wreck
+ghost-role-information-salvage-kangaroo-description = Defend the loot inside the salvage wreck!
+
+ghost-role-information-space-spider-name = Space spider
+ghost-role-information-space-spider-description =  Space spiders are just as aggressive as regular spiders, feed.
+
+ghost-role-information-salvage-spider-name = Space spider on salvage wreck
+ghost-role-information-salvage-spider-description = Space spiders are just as aggressive as regular spiders, feed.
+
+ghost-role-information-guardian-name = Guardian
+ghost-role-information-guardian-description = Listen to your owner. Don't tank damage. Punch people hard.
+
+ghost-role-information-holoparasite-name = Holoparasite
+ghost-role-information-holoparasite-description = Listen to your owner. Don't tank damage. Punch people hard.
+
+ghost-role-information-ifrit-name = Ifrit
+ghost-role-information-ifrit-description = Listen to your owner. Don't tank damage. Punch people hard.
+
+ghost-role-information-space-dragon-name = Space dragon
+ghost-role-information-space-dragon-description = Call in 3 carp rifts and take over this quadrant! You have only 5 minutes in between each rift before you will disappear.
+
+ghost-role-information-cluwne-name = Cluwne
+ghost-role-information-cluwne-description = Become a pitiful cluwne, your only goal in life is to find a sweet release from your suffering (usually by being beaten to death). A cluwne is not an antagonist but may defend itself. Crewmembers may murder cluwnes freely.
+
+ghost-role-information-skeleton-pirate-name = Skeleton Pirate
+ghost-role-information-skeleton-pirate-description = Cause chaos and loot the station for treasure.
+
+ghost-role-information-skeleton-biker-name = Skeleton Biker
+ghost-role-information-skeleton-biker-description = Ride around on your sweet ride.
+
+ghost-role-information-onestar-mecha-name = Onestar Mecha
+ghost-role-information-onestar-mecha-description = You are an experimental mecha created by who-knows-what, all you know is that you have weapons and you detect fleshy moving targets nearby...
+ghost-role-information-onestar-mecha-rules = Use your weapons to cause havoc. You are an antagonist.
+
+ghost-role-information-remilia-name = Remilia, the chaplain's familiar
+ghost-role-information-remilia-description = Obey your master. Eat fruit.
+ghost-role-information-remilia-rules = You are an intelligent fruit bat. Follow the chaplain around. Don't cause any trouble unless the chaplain tells you to.
+
+ghost-role-information-cerberus-name = Cerberus, Evil Famili
+ghost-role-information-cerberus-description = Obey your master. Spread chaos.
+ghost-role-information-cerberus-rules = You are an intelligent, demonic dog. Try to help the chaplain and any of his flock. As an antagonist, you're otherwise unrestrained.
+
+ghost-role-information-ert-leader-name = ERT Leader
+ghost-role-information-ert-leader-description = Lead a team of specialists to resolve the stations issues.
+
+ghost-role-information-ert-janitor-name = ERT Janitor
+ghost-role-information-ert-janitor-description = Assist with custodial efforts to resolve the stations issues.
+
+ghost-role-information-ert-engineer-name = ERT Engineer
+ghost-role-information-ert-engineer-description = Assist with engineering efforts to resolve the stations issues.
+
+ghost-role-information-ert-security-name = ERT Security
+ghost-role-information-ert-security-description = Assist with security efforts to resolve the stations issues.
+
+ghost-role-information-ert-medical-name = ERT Medical
+ghost-role-information-ert-medical-description = Assist with medicaling efforts to resolve the stations issues.
+
+ghost-role-information-cburn-agent-name = CBURN Agent
+ghost-role-information-cburn-agent-description = A highly trained CentCom agent, capable of dealing with various threats.
+
+ghost-role-information-centcom-official-name = CentCom official
+ghost-role-information-centcom-official-description = Inspect the station, jot down performance reviews for heads of staff, bug the Captain.

Nyanotrasen/Nyanotrasen

Resources/Locale/en-US/ghost/roles/ghost-role-component.ftl
@@ -35,3 +35,121 @@ ghost-role-information-hamlet-description = Lives in the station bridge, has a b
 ghost-role-information-slimes-name = Slime
 ghost-role-information-slimes-description = An ordinary slime with no special needs or interests. He just lives.
 ghost-role-information-angry-slimes-description = Everything around you irritates your instincts, destroy them!
+
+ghost-role-information-xeno-name = Xeno
+ghost-role-information-xeno-description = You are a xeno, co-operate with your hive to kill all crewmembers!
+ghost-role-information-xeno-rules = You are an antagonist, smack, slash, and wack!
+
+ghost-role-information-revenant-name = Revenant
+ghost-role-information-revenant-description = You are a Revenant. Use your powers to harvest souls and unleash chaos upon the crew. Unlock new abilities with the essence you harvest.
+ghost-role-information-revenant-rules = You are an antagonist, harvest, defile, and drive the crew insane.
+
+ghost-role-information-kangaroo-name = Kangaroo
+ghost-role-information-kangaroo-description = You're a kangaroo! Do whatever kangaroos do.
+
+ghost-role-information-monkey-name = Monkey
+ghost-role-information-monkey-description = Ook ook!
+
+ghost-role-information-rat-king-name = Rat King
+ghost-role-information-rat-king-description = You are the Rat King, scavenge food in order to produce rat minions to do your bidding.
+ghost-role-information-rat-king-rules = You are an antagonist, scavenge, attack, and grow your hoard!
+
+ghost-role-information-rat-servant-name = Rat Servant
+ghost-role-information-rat-servant-description = You are a Rat Servant. You must follow your king's orders.
+ghost-role-information-rat-servant-rules = You are an antagonist, scavenge, attack, and serve your king!
+
+ghost-role-information-salvage-carp-name = Space carp on salvage wreck
+ghost-role-information-salvage-carp-description = Defend the loot inside the salvage wreck!
+
+ghost-role-information-sentient-carp-name = Sentient Carp
+ghost-role-information-sentient-carp-description = Help the dragon flood the station with carps!
+
+ghost-role-information-willow-name = Willow the Kangaroo
+ghost-role-information-willow-description = You're a kangaroo named Willow! Willow likes to box.
+
+ghost-role-information-space-tick-name = Space tick
+ghost-role-information-space-tick-description = Wreak havoc on the station!
+
+ghost-role-information-salvage-tick-name = Space tick on salvage wreck
+ghost-role-information-salvage-tick-description = Defend the loot inside the salvage wreck!
+
+ghost-role-information-honkbot-name = Honkbot
+ghost-role-information-honkbot-description = An artificial being of pure evil.
+
+ghost-role-information-jonkbot-name = Jonkbot
+ghost-role-information-jonkbot-description = An artificial being of pure evil.
+
+ghost-role-information-mimebot-name = Mimebot
+ghost-role-information-mimebot-description = A Mimebot, act like a mime but dont act like a greytider.
+
+ghost-role-information-space-bear-name = Space bear
+ghost-role-information-space-bear-description = You're a bear! Do bear things.
+
+ghost-role-information-salvage-bear-name = Space bear on salvage wreck
+ghost-role-information-salvage-bear-description = Defend the loot inside the salvage wreck!
+
+ghost-role-information-space-kangaroo-name = Space kangaroo
+ghost-role-information-space-kangaroo-description = You're a kangaroo! Do kangaroo things.
+
+ghost-role-information-salvage-kangaroo-name = Space kangaroo on salvage wreck
+ghost-role-information-salvage-kangaroo-description = Defend the loot inside the salvage wreck!
+
+ghost-role-information-space-spider-name = Space spider
+ghost-role-information-space-spider-description =  Space spiders are just as aggressive as regular spiders, feed.
+
+ghost-role-information-salvage-spider-name = Space spider on salvage wreck
+ghost-role-information-salvage-spider-description = Space spiders are just as aggressive as regular spiders, feed.
+
+ghost-role-information-guardian-name = Guardian
+ghost-role-information-guardian-description = Listen to your owner. Don't tank damage. Punch people hard.
+
+ghost-role-information-holoparasite-name = Holoparasite
+ghost-role-information-holoparasite-description = Listen to your owner. Don't tank damage. Punch people hard.
+
+ghost-role-information-ifrit-name = Ifrit
+ghost-role-information-ifrit-description = Listen to your owner. Don't tank damage. Punch people hard.
+
+ghost-role-information-space-dragon-name = Space dragon
+ghost-role-information-space-dragon-description = Call in 3 carp rifts and take over this quadrant! You have only 5 minutes in between each rift before you will disappear.
+
+ghost-role-information-cluwne-name = Cluwne
+ghost-role-information-cluwne-description = Become a pitiful cluwne, your only goal in life is to find a sweet release from your suffering (usually by being beaten to death). A cluwne is not an antagonist but may defend itself. Crewmembers may murder cluwnes freely.
+
+ghost-role-information-skeleton-pirate-name = Skeleton Pirate
+ghost-role-information-skeleton-pirate-description = Cause chaos and loot the station for treasure.
+
+ghost-role-information-skeleton-biker-name = Skeleton Biker
+ghost-role-information-skeleton-biker-description = Ride around on your sweet ride.
+
+ghost-role-information-onestar-mecha-name = Onestar Mecha
+ghost-role-information-onestar-mecha-description = You are an experimental mecha created by who-knows-what, all you know is that you have weapons and you detect fleshy moving targets nearby...
+ghost-role-information-onestar-mecha-rules = Use your weapons to cause havoc. You are an antagonist.
+
+ghost-role-information-remilia-name = Remilia, the chaplain's familiar
+ghost-role-information-remilia-description = Obey your master. Drink blood.
+ghost-role-information-remilia-rules = You are an intelligent vampire bat. Follow the chaplain around. Don't cause any trouble unless the chaplain tells you to.
+
+ghost-role-information-cerberus-name = Cerberus, Evil Famili
+ghost-role-information-cerberus-description = Obey your master. Spread chaos.
+ghost-role-information-cerberus-rules = You are an intelligent, demonic dog. Try to help the chaplain and any of his flock. As an antagonist, you're otherwise unrestrained.
+
+ghost-role-information-ert-leader-name = ERT Leader
+ghost-role-information-ert-leader-description = Lead a team of specialists to resolve the stations issues.
+
+ghost-role-information-ert-janitor-name = ERT Janitor
+ghost-role-information-ert-janitor-description = Assist with custodial efforts to resolve the stations issues.
+
+ghost-role-information-ert-engineer-name = ERT Engineer
+ghost-role-information-ert-engineer-description = Assist with engineering efforts to resolve the stations issues.
+
+ghost-role-information-ert-security-name = ERT Security
+ghost-role-information-ert-security-description = Assist with security efforts to resolve the stations issues.
+
+ghost-role-information-ert-medical-name = ERT Medical
+ghost-role-information-ert-medical-description = Assist with medicaling efforts to resolve the stations issues.
+
+ghost-role-information-cburn-agent-name = CBURN Agent
+ghost-role-information-cburn-agent-description = A highly trained CentCom agent, capable of dealing with various threats.
+
+ghost-role-information-centcom-official-name = CentCom official
+ghost-role-information-centcom-official-description = Inspect the station, jot down performance reviews for heads of staff, bug the Captain.