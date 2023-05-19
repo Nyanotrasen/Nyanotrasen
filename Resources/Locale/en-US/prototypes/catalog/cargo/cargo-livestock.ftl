RebelledCombine/Zona-14

add lizard crate (#14337)

Resources/Locale/en-US/prototypes/catalog/cargo/cargo-livestock.ftl
@@ -42,3 +42,6 @@ ent-LivestockPenguin = { ent-CrateNPCPenguin }
 
 ent-LivestockSnake = { ent-CrateNPCSnake }
     .desc = { ent-CrateNPCSnake.desc }
+
+ent-LivestockSnake = { ent-CrateNPCLizard }
+    .desc = { ent-CrateNPCLizard.desc }

Nyanotrasen/Nyanotrasen

Resources/Locale/en-US/prototypes/catalog/cargo/cargo-livestock.ftl
@@ -45,3 +45,6 @@ ent-LivestockPenguin = { ent-CrateNPCPenguin }
 
 ent-LivestockSnake = { ent-CrateNPCSnake }
     .desc = { ent-CrateNPCSnake.desc }
+
+ent-LivestockLizard = { ent-CrateNPCLizard }
+    .desc = { ent-CrateNPCLizard.desc }