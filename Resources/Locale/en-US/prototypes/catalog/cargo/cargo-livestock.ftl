RebelledCombine/Zona-14

[entities] Kangaroos, boxer improvements, boxing bell and hamlet updates. (#15546)
* kangaroo

* fixes

* fuix

* fixes and rotational sprites for bell

* why wont u work, stupid rsi thingy

* attempt to fix

* now hsould work

* fix bell

* fix the bell

* bell make noise on hitting it

* add faction simple hostile to space kangaroo

* added boxing kangaroo spawner comes with gloves and agressive

Resources/Locale/en-US/prototypes/catalog/cargo/cargo-livestock.ftl
@@ -45,3 +45,6 @@ ent-LivestockSnake = { ent-CrateNPCSnake }
 
 ent-LivestockSnake = { ent-CrateNPCLizard }
     .desc = { ent-CrateNPCLizard.desc }
+
+ent-LivestockSnake = { ent-CrateNPCKangaroo }
+    .desc = { ent-CrateNPCKangaroo.desc }

Nyanotrasen/Nyanotrasen

Resources/Locale/en-US/prototypes/catalog/cargo/cargo-livestock.ftl
@@ -48,3 +48,6 @@ ent-LivestockSnake = { ent-CrateNPCSnake }
 
 ent-LivestockLizard = { ent-CrateNPCLizard }
     .desc = { ent-CrateNPCLizard.desc }
+
+ent-LivestockKangaroo = { ent-CrateNPCKangaroo }
+    .desc = { ent-CrateNPCKangaroo.desc }