6d09d5b3c597c2d1c4c5e8a77c508bbf1489c92d
Qulibly <52052459+Qulibly@users.noreply.github.com>
2023-05-22 01:05:35 +0200

    Fixed documentation comment
    Co-authored-by: Vordenburg <114301317+Vordenburg@users.noreply.github.com>

--- Content.Server/Nyanotrasen/Shuttles/Components/FTLWhitelistComponent.cs
+++ Content.Server/Nyanotrasen/Shuttles/Components/FTLWhitelistComponent.cs
@@ -1,6 +1,6 @@
 namespace Content.Server.Shuttles.Components;
 /// <summary>
-//Allows a grid to ignore FTL mass limit. Implemented to allow functioning of Spaceships, large shuttle grids/maps without issues.
+/// Allows a grid to ignore FTL mass limit. Implemented to allow functioning of Spaceships, large shuttle grids/maps without issues.
 /// </summary>
 [RegisterComponent]
 public sealed class FTLWhitelistComponent : Component
