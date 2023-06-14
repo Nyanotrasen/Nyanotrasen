3e6e712fd5e5f54509a567a30072127fbb19790a
Debug <49997488+DebugOk@users.noreply.github.com>
2023-06-14 13:38:53 +0200

    Adding a small part of the ghost bar system early (#51)
    * Add player spawner
    
    * Add in reviews
    
    * Update to use custom spawner type
    
    * This should work
    
    * Update GhostRoleSystem.cs

---
+++ Content.Server/DeltaV/Ghost/Roles/GhostRoleCharacterSystem.cs
@@ -0,0 +1,62 @@
+﻿using Content.Server.Ghost.Roles;
+using Content.Server.Ghost.Roles.Components;
+using Content.Server.Ghost.Roles.Events;
+using Content.Server.Mind.Components;
+using Content.Server.Preferences.Managers;
+using Content.Server.Station.Systems;
+using Content.Shared.Preferences;
+using JetBrains.Annotations;
+using Robust.Server.GameObjects;
+
+namespace Content.Server.DeltaV.Ghost.Roles
+{
+    [UsedImplicitly]
+    public sealed class GhostRoleCharacterSystem : EntitySystem
+    {
+        [Dependency] private readonly TransformSystem _transform = default!;
+        [Dependency] private readonly IServerPreferencesManager _prefs = default!;
+        [Dependency] private readonly IEntityManager _entityManager = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<GhostRoleCharacterSpawnerComponent, TakeGhostRoleEvent>(OnSpawnerTakeCharacter);
+        }
+        private void OnSpawnerTakeCharacter( EntityUid uid, GhostRoleCharacterSpawnerComponent component,
+            ref TakeGhostRoleEvent args)
+        {
+            if (!TryComp(uid, out GhostRoleComponent? ghostRole) ||
+                ghostRole.Taken)
+            {
+                args.TookRole = false;
+                return;
+            }
+
+            var character = (HumanoidCharacterProfile) _prefs.GetPreferences(args.Player.UserId).SelectedCharacter;
+
+            var mob = _entityManager.System<StationSpawningSystem>()
+                .SpawnPlayerMob(Transform(uid).Coordinates, null, character, null);
+            _transform.AttachToGridOrMap(mob);
+
+            var spawnedEvent = new GhostRoleSpawnerUsedEvent(uid, mob);
+            RaiseLocalEvent(mob, spawnedEvent);
+
+            mob.EnsureComponent<MindComponent>();
+
+            _entityManager.System<GhostRoleSystem>().GhostRoleInternalCreateMindAndTransfer(args.Player, uid, mob, ghostRole);
+
+            if (++component.CurrentTakeovers < component.AvailableTakeovers)
+            {
+                args.TookRole = true;
+                return;
+            }
+
+            ghostRole.Taken = true;
+
+            if (component.DeleteOnSpawn)
+                QueueDel(uid);
+
+            args.TookRole = true;
+        }
+    }
+}
