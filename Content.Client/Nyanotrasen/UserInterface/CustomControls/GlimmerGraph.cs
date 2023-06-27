6ee199da0f0ec3392637a65be564fc65e865e7f9
Rane <elijahrane@gmail.com>
2023-06-26 23:01:36 -0400

    i love ui :smile:

--- Content.Client/Nyanotrasen/UserInterface/CustomControls/GlimmerGraph.cs
+++ Content.Client/Nyanotrasen/UserInterface/CustomControls/GlimmerGraph.cs
@@ -20,8 +20,10 @@ namespace Content.Client.Nyanotrasen.UserInterface.CustomControls
         protected override void Draw(DrawingHandleScreen handle)
         {
             base.Draw(handle);
+            var box = new UIBox2((15, 230), (465, 30));
+            handle.DrawRect(box, Color.FromHex("#424245"));
             var texture = _resourceCache.GetTexture("/Textures/Interface/glimmerGraph.png");
-            handle.DrawTexture(texture, (0, 50));
+            handle.DrawTexture(texture, (15, 30));
 
             if (_glimmer.Count < 2)
                 return;
@@ -31,12 +33,12 @@ namespace Content.Client.Nyanotrasen.UserInterface.CustomControls
             var i = 0;
             while (i + 1 < _glimmer.Count)
             {
-                Vector2 vector1 = (i * spacing, 250 - _glimmer[i] / 5);
-                Vector2 vector2 = ((i + 1) * spacing, 250 - _glimmer[i + 1] / 5);
+                Vector2 vector1 = (15 + i * spacing, 230 - _glimmer[i] / 5);
+                Vector2 vector2 = (15 + (i + 1) * spacing, 230 - _glimmer[i + 1] / 5);
                 handle.DrawLine(vector1, vector2, Color.FromHex("#A200BB"));
                 handle.DrawLine(vector1 + (0, 1), vector2 + (0, 1), Color.FromHex("#A200BB"));
                 handle.DrawLine(vector1 - (0, 1), vector2 - (0, 1), Color.FromHex("#A200BB"));
-                handle.DrawLine((i * spacing, 250), (i * spacing, 50), Color.FromHex("#686868"));
+                handle.DrawLine((15 + i * spacing, 230), (15 + i * spacing, 30), Color.FromHex("#686868"));
                 i++;
             }
         }
