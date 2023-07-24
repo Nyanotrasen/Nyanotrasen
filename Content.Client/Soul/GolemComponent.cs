04a73679b8080f81001e61622246a081132ea903
Vordenburg <114301317+Vordenburg@users.noreply.github.com>
2023-07-24 12:19:48 -0400

    Prevent golems from shooting guns again (#1724)
    * Move out Nyano localization
    
    * Prevent golems from using guns again

---
+++ Content.Client/Soul/GolemComponent.cs
@@ -0,0 +1,6 @@
+namespace Content.Shared.Soul;
+
+[RegisterComponent]
+public sealed class GolemComponent : SharedGolemComponent
+{
+}
---
+++ Content.Shared/Nyanotrasen/Soul/GunHeldByGolemComponent.cs
@@ -0,0 +1,6 @@
+namespace Content.Shared.Soul;
+
+[RegisterComponent]
+public sealed class GunHeldByGolemComponent : Component
+{
+}
