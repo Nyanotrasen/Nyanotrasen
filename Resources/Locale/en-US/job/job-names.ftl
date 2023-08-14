teecoding/Zona-14

Paramedic role, redone (#15439)
Co-authored-by: MagnusCrowe <whiterider1988@yahoo.com>

Resources/Locale/en-US/job/job-names.ftl
@@ -10,6 +10,7 @@ job-name-rd = Research Director
 job-name-psychologist = Psychologist
 job-name-intern = Medical Intern
 job-name-doctor = Medical Doctor
+job-name-paramedic = Paramedic
 job-name-cmo = Chief Medical Officer
 job-name-chemist = Chemist
 job-name-technical-assistant = Technical Assistant
@@ -73,6 +74,7 @@ JobMedicalDoctor = Medical doctor
 JobMedicalIntern = Medical intern
 JobMime = Mime
 JobMusician = Musician
+JobParamedic = Paramedic
 JobPassenger = Passenger
 JobPsychologist = Psychologist
 JobQuartermaster = Quartermaster

Nyanotrasen/Nyanotrasen

(cherry picked from commit 332510df3b60261061e900b3e98af4d9b40fd13f)

Resources/Locale/en-US/job/job-names.ftl
@@ -1,4 +1,4 @@
-﻿job-name-warden = warden
+job-name-warden = warden
 job-name-security = security officer
 job-name-cadet = security cadet
 job-name-hos = head of security
@@ -8,6 +8,7 @@ job-name-rd = research director
 job-name-psychologist = psychologist
 job-name-intern = medical intern
 job-name-doctor = medical doctor
+job-name-paramedic = paramedic
 job-name-cmo = chief medical officer
 job-name-chemist = chemist
 job-name-technical-assistant = technical assistant